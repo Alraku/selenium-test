8ed7afa6cf7a8bd64a36377d5eabe6824fcd131f
Alraku <ekoalraku@gmail.com>
2022-05-14 21:20:48 +0200

    Added Fixtures functionality, works for edge and safari

--- __main__test.py
+++ __main__test.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 import pytest
 
 from selenium import webdriver
@@ -5,16 +6,28 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-@pytest.fixture()
-def test_setup():
-    global driver
-    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
-    driver.get("https://www.onet.pl")
+class TestClass:
 
-    yield
-    driver.quit()
+    @pytest.fixture(params=["edge", "safari"], scope='class')
+    def driver(self, request):
+        if request.param == "edge":
+            driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()))
+        if request.param == "safari":
+            driver = webdriver.Safari(service = Service(executable_path='/usr/bin/safaridriver'))
+
+        yield driver
+        driver.quit()
 
 
-def test_check_website_title(test_setup):
-    print(driver.title)
-    assert "Onet" in driver.title, "Assertion Failed"
+    @pytest.mark.order(2)
+    def test_check_website_title(self, driver):
+        driver.get("https://www.onet.pl")
+        print(driver.title)
+        assert "Onet" in driver.title, "Assertion Failed"
+
+
+    @pytest.mark.order(1)
+    def test_check_website_title_2(self, driver):
+        driver.get("https://www.google.com")
+        print(driver.title)
+        assert "Google" in driver.title, "Assertion Failed"
