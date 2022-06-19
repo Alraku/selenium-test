a68e19208da6b9a81068e2c6827c59d95abad4e3
Alraku <ekoalraku@gmail.com>
2022-06-19 20:23:27 +0200

    Changed driver to connect to remote selenium grid

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,16 +8,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.safari.options import Options as SafariOptions
+
 
 
 #@pytest.fixture(params=["edge", "safari"], scope='class')
 @pytest.fixture(scope='class')
 def setup(request, browser, url):
     if browser == "edge":
-        driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()))
+        options = EdgeOptions()
+        # driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()))
+        driver = webdriver.Remote(command_executor='http://192.168.1.10:4444/wd/hub', options = options)
     elif browser == "chrome":
-        driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+        options = ChromeOptions()
+        # driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+        driver = webdriver.Remote(command_executor='http://192.168.1.10:4444/wd/hub', options = options)
     elif browser == "safari":
         safari_options = SafariOptions()
         driver = webdriver.Safari(service = Service(executable_path='/usr/bin/safaridriver'), options=safari_options)
