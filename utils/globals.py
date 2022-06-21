ffb76999733ce7cc2c397efb827f8fd94dd79c61
Alraku <ekoalraku@gmail.com>
2022-06-21 22:11:25 +0200

    Added new global values

--- utils/globals.py
+++ utils/globals.py
@@ -2,7 +2,11 @@ import pathlib
 
 ROOT_DIR = str(pathlib.Path(__file__).parents[1])
 
+SEL_GRID_URL = 'http://192.168.1.10:4444/wd/hub'
+
 BASE_URL = 'https://www.olx.pl'
+
+SAFARI_DRIVER_PATH = '/usr/bin/safaridriver'
 
 TEST_EMAIL = 'testautomation2@proton.me'
 
