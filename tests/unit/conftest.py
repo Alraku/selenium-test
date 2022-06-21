33bc7d6c23073f63d3b79a3a55101b8833e847c2
Alraku <ekoalraku@gmail.com>
2022-06-21 22:11:13 +0200

    Updated form of conftest.py to always use remote server with browser

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -13,21 +13,27 @@ from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.safari.options import Options as SafariOptions
 
 
-
-#@pytest.fixture(params=["edge", "safari"], scope='class')
 @pytest.fixture(scope='class')
-def setup(request, browser, url):
+def setup(request, browser, mode):
     if browser == "edge":
         options = EdgeOptions()
         # driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()))
-        driver = webdriver.Remote(command_executor='http://192.168.1.10:4444/wd/hub', options = options)
+        driver = webdriver.Remote(
+            command_executor = globals.SEL_GRID_URL,
+            options = options)
+
     elif browser == "chrome":
         options = ChromeOptions()
         # driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-        driver = webdriver.Remote(command_executor='http://192.168.1.10:4444/wd/hub', options = options)
+        driver = webdriver.Remote(
+            command_executor = globals.SEL_GRID_URL,
+            options = options)
+            
     elif browser == "safari":
         safari_options = SafariOptions()
-        driver = webdriver.Safari(service = Service(executable_path='/usr/bin/safaridriver'), options=safari_options)
+        driver = webdriver.Safari(service = Service(
+            executable_path = globals.SAFARI_DRIVER_PATH),
+            options = safari_options)
 
     driver.maximize_window()
     request.cls.driver = driver
@@ -39,19 +45,21 @@ def setup(request, browser, url):
     driver.quit()
 
 
-#this will get the value of CLI/Hooks
+# This will get the value of CLI/Hooks
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome")
-    parser.addoption("--url")
+    # safari edge chrome
+    parser.addoption("--browser", default="edge") 
+    # local remote
+    parser.addoption("--mode", default="local")
 
 
-#This will return the browser value to setup method
+# This will return the browser value to setup method
 @pytest.fixture(scope="class", autouse=True)
 def browser(request):
     return request.config.getoption("--browser")
 
 
-#This will return the url value to setup method
+# This will return the mode value to setup method
 @pytest.fixture(scope="class", autouse=True)
-def url(request):
-    return request.config.getoption("--url")
+def mode(request):
+    return request.config.getoption("--mode")
