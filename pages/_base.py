eb6e77e144a133a4861527d674a136f61c2f2d58
Alraku <ekoalraku@gmail.com>
2022-07-03 13:07:55 +0200

    Added base page class in order to handle driver and logger instances

---
+++ pages/_base.py
@@ -0,0 +1,8 @@
+import logging
+
+class BasePage(object):
+    """Base class to initialize the base page that will be called from all pages"""
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.logger = logging.getLogger(__name__)
