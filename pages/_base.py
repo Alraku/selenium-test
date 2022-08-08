2ae29600c5dbf9c9cb542a5cc5ed405cbf021a31
Alraku <ekoalraku@gmail.com>
2022-08-08 22:22:22 +0200

    Moved from frameworks to BasePage

--- pages/_base.py
+++ pages/_base.py
@@ -1,6 +1,12 @@
-import imp
+import time
 import logging
+
 from utils.globals import BASE_URL
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 
 class BasePage(object):
@@ -14,3 +20,26 @@ class BasePage(object):
 
     def open(self):
         self.driver.get(self.url_path)
+
+
+    def find_element(self, locator: tuple[By, str], timeout: int = 3) -> WebElement:
+        (locator_type, locator_value) = locator
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((
+                    locator_type, locator_value)))
+            return element
+
+        except TimeoutException as Exception:
+            self.logger.warning("Element not found in desired time.")
+            raise Exception
+
+
+    def page_scroll(self, height) -> None:
+        self.logger.info(f"Scrolling page to Y: {height}")
+        self.driver.execute_script(f"window.scrollTo(0, {height})")
+
+
+    def wait(self, timeout) -> None:
+        self.logger.info(f"Waiting: {timeout} seconds.")
+        time.sleep(timeout)
