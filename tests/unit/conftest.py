d861f69d297f3a534e5d23d9195515f8c96e7bf3
Alraku <ekoalraku@gmail.com>
2022-08-09 11:09:18 +0200

    Moved initialization of webdriver to separate file

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,73 +1,49 @@
 import os
 import pytest
 import logging
-import datetime
-import utils.globals as globals
 
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
-from selenium.webdriver.safari.options import Options as SafariOptions
+from datetime import datetime
+from tests.unit._webdriver import WebDriver
 
 logger = logging.getLogger(__name__)
-start_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d__%H-%M-%S')
+start_time = datetime.strftime(datetime.now(), '%Y-%m-%d__%H-%M-%S')
 
 
 @pytest.hookimpl
 def pytest_runtest_setup(item):
     logging_plugin = item.config.pluginmanager.get_plugin("logging-plugin")
-    logging_plugin.set_log_path(os.path.join('logs', f'test_session_{start_time}', f'{item.name}.log'))
+    logging_plugin.set_log_path(
+        os.path.join('logs', f'test_session_{start_time}', f'{item.name}.log'))
 
 
 @pytest.fixture(scope='function', autouse=True)
 def test_log(request):
-    logger.info(f"Execution of Test Case: {request.node.name.upper()} has started.")
+    test_case_name = request.node.name.upper()
+    logger.info(f"Execution of Test Case: {test_case_name} has started.")
 
     def fin():
-        logger.info(f"Execution of Test Case: {request.node.name.upper()} has ended.")
+        logger.info(f"Execution of Test Case: {test_case_name} has ended.")
 
     request.addfinalizer(fin)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="safari")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
 
 
 @pytest.fixture(scope='class')
 def setup(request, browser):
     logger.info("Preparing instance of Webdriver")
+    driver = WebDriver().get_webdriver(browser)
 
-    if browser == "edge":
-        options = EdgeOptions()
-        driver = webdriver.Remote(
-            command_executor = globals.SEL_GRID_URL,
-            options = options)
+    if request.cls is not None:
+        request.cls.driver = driver
 
-    elif browser == "chrome":
-        options = ChromeOptions()
-        driver = webdriver.Remote(
-            command_executor = globals.SEL_GRID_URL,
-            options = options)
-            
-    elif browser == "safari":
-        options = SafariOptions()
-        driver = webdriver.Safari(service = Service(
-            executable_path = globals.SAFARI_DRIVER_PATH),
-            options = options)
-
-    driver.maximize_window()
-    request.cls.driver = driver
-    
     yield driver
-
     logger.info("Closing instance of Webdriver.")
     driver.quit()
-
-
-# This will get the value of CLI/Hooks
-def pytest_addoption(parser):
-    parser.addoption("--browser", default="safari") 
-
-
-# This will return the browser value to setup method
-@pytest.fixture(scope="class", autouse=True)
-def browser(request):
-    return request.config.getoption("--browser")
