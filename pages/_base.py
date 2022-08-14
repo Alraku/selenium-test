d550b413ac2c24cc0f2581270ae658ed716617d8
Alraku <ekoalraku@gmail.com>
2022-08-14 23:20:52 +0200

    Code refactor

--- pages/_base.py
+++ pages/_base.py
@@ -2,15 +2,17 @@ import time
 import logging
 
 from utils.globals import BASE_URL
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 
 
 class BasePage(object):
-    """Base class to initialize the base page that will be called from all pages"""
+    """
+    Base class to initialize the base page that will be called from all pages.
+    """
 
     def __init__(self, driver, url_path: str):
         self.driver = driver
@@ -20,7 +22,8 @@ class BasePage(object):
     def open(self):
         self.driver.get(self.url_path)
 
-    def find_element(self, locator: tuple[By, str], timeout: int = 3) -> WebElement:
+    def find_element(
+            self, locator: tuple[By, str], timeout: int = 3) -> WebElement:
         (locator_type, locator_value) = locator
         try:
             element = WebDriverWait(self.driver, timeout).until(
