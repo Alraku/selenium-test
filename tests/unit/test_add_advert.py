147e0f2e6677fdcff962f919093db451e0a6da50
Alraku <ekoalraku@gmail.com>
2022-08-19 22:11:15 +0200

    Code refactor, method cleanup

--- tests/unit/test_add_advert.py
+++ tests/unit/test_add_advert.py
@@ -1,30 +1,24 @@
 import pytest
 import utils.globals as globals
 
-from pages.page_add_advert import PageAddAdvert
 from pages.page_login import PageLogin
-from pages.page_home import PageHome
+from pages.page_add_advert import PageAddAdvert
 from data.test_data import testdata_advert_form_fields
 
 
 @pytest.mark.usefixtures("setup")
 class TestAddAdvert(object):
 
-    @pytest.fixture()
+    @pytest.fixture(autouse=True)
     def class_setup(self):
         self.page_login = PageLogin(self.driver)
-        self.page_home = PageHome(self.driver)
         self.page_add_advert = PageAddAdvert(self.driver)
         self.page_login.load_cookie()
-        self.driver.get(globals.BASE_URL)
-        self.page_home.click_add_advert()
-        # self.driver.get(globals.BASE_URL + '/nowe-ogloszenie')
+        self.page_add_advert.open()
 
-    @pytest.mark.order(1)
     @pytest.mark.parametrize('advert', testdata_advert_form_fields)
-    def test_add_advert_basic(self, class_setup, advert):
-        self.page_add_advert.advert = advert
-        self.page_add_advert.fill_in_fields()
-        self.page_add_advert.submit_advert()
+    def test_add_advert(self, advert):
+        self.page_add_advert.fill_in(advert)
+        self.page_add_advert.submit()
         self.driver.get(globals.BASE_URL + '/mojolx/waiting/')
-        assert self.page_add_advert.verify_new_advert() == advert.get('title')
+        assert self.page_add_advert.is_advert_visible()
